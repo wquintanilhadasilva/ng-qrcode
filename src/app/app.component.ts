1e6b85fc9c66204d68e2ad6c79d4ff666d2504b5
Wedson Quintanilha da Silva <wedson.silva@basis.com.br>
2022-10-03 16:46:01 -0300

    adicionar src

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { NgxScannerQrcodeService, SelectedFiles } from 'ngx-scanner-qrcode';
 
 @Component({
   selector: 'app-root',
@@ -7,4 +8,33 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'qrcode';
+
+
+
+  /**
+   * Properties
+   * CanvasRenderingContext2D
+   * https://developer.mozilla.org/en-US/docs/Web/API/CanvasRenderingContext2D
+   */
+  public config: Object = {
+    text: { fillStyle: 'red' },
+    frame: { strokeStyle: 'red' }
+  };
+
+  public selectedFiles: SelectedFiles[] = [];
+
+  constructor(private qrcode: NgxScannerQrcodeService) { }
+
+  public onError(e: any): void {
+    alert(e);
+  }
+
+  public handle(action: any, fn: string): void {
+    action[fn]().subscribe((res: boolean) =>  console.log(fn + ': ' + res));
+  }
+
+  public onSelects(files: any) {
+    this.qrcode.toBase64(files, this.selectedFiles).subscribe(res => this.selectedFiles = res);
+  }
+
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,13 +4,16 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 
+import { NgxScannerQrcodeModule } from 'ngx-scanner-qrcode';
+
 @NgModule({
   declarations: [
     AppComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    NgxScannerQrcodeModule
   ],
   providers: [],
   bootstrap: [AppComponent]
